e5cbc848087c5cfffdbbeb52ef44c2b897b480a1
benpark <benpark508@gmail.com>
2023-09-12 19:04:25 -0500

    ewfew

---
+++ backend-service/database.py
@@ -0,0 +1,5 @@
+#https://prod.liveshare.vsengsaas.visualstudio.com/join?83997568C9AD9AFC2310CF40C26DC0330799
+import mysql.connector
+
+cnx = mysql.connector.connect(database= "__main__")
+cursor = cnx.cursor()
