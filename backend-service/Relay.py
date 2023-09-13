06b950e51bab1e97f623e53051a2b44bf59d268b
Parthiv Shah <parthivjs12@gmail.com>
2023-09-13 17:28:32 -0500

    Relay Class contructor

--- backend-service/Relay.py
+++ backend-service/Relay.py
@@ -1,18 +1,14 @@
 import RPi.GPIO as GPIO
 
 class Relay:
-    def __init__(self, id: int):
+    def __init__(self, id: int, state: bool = False):
         self.id = id
-        self.state = False
+        self.state = state
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.id, GPIO.OUT)
 
         GPIO.output(self.id, self.state)
-    
-    def __init__(self, id: int, state: bool):
-        self(id)
-        self.setState(state)
 
     def setState(self, state: bool):
         self.state = state
