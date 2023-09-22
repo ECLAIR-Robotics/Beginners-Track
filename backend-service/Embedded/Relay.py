cf7e077cf2b36966206714e78787824b2a45956d
NathanChase22 <ntc477@utexas.edu>
2023-09-22 18:32:03 -0500

    just pushing some stuff

--- backend-service/Embedded/Relay.py
+++ backend-service/Embedded/Relay.py
@@ -1,6 +1,32 @@
+#make our imports here
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges."  
+    + "You can achieve this by using 'sudo' to run your script")
+
 # This will be where we will create our relay class 
-class Relay: 
+class Relay:
+    #lets make a list of tuples, each tuple will contain a GPIO pin and then it's status
+    
+    #make empty list
+    chan_list = []
+    #run for loop
+    for x in range(0,26):
+        #make our tubles
+        chan_list.append((x+2, 0))
+
+
+    #define our Relay object
     def __init__(self):
-        #define our Relay object
-        #which data structure should we use
+
+        #GPIO already handles the pins as long as we have the list
+        
+        #this designates our GPIO pins to correspond to the pins
+        GPIO.setmode(GPIO.BOARD)
+
+
         print("")
+
+
+    
