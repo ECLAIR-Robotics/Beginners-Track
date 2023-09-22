03392028f2ef6740fe753d6ad9d4fde60905bd8d
Adi <adi.sr@outlook.com>
2023-09-22 17:58:16 -0500

    Added a button grid and header. Need to style later

---
+++ frontend-service/my-app/src/ButtonGrid.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './ButtonGrid.css';
+
+function ButtonGrid() {
+  return (
+    <div>
+        <header>
+            <h1 id="Title">Hello World</h1>
+        </header>
+
+        <div className='Grid-Container'>
+            <div className="row">
+                <div className ="col"><button>1</button></div>
+                <div className ="col"><button>2</button></div>
+            </div>
+
+            <div className="row">
+                <div className ="col"><button>1</button></div>
+                <div className ="col"><button>2</button></div>
+            </div>
+
+            <div className="row">
+                <div className ="col"><button>1</button></div>
+                <div className ="col"><button>2</button></div>
+            </div>
+        </div> 
+    </div>
+  );
+}
+
+export default ButtonGrid;
