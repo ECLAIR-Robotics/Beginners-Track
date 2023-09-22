74fdb65834b4feacaea906eeca7ab8152d3af080
PersonOOO <82835562+PersonOOO@users.noreply.github.com>
2023-09-22 18:14:46 -0500

    added frontend code

--- frontend-service/my-app/src/App.tsx
+++ frontend-service/my-app/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,11 +15,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
   );
-}
+} 
 
 export default App;
