edf1d0ed34f0bc47ea64dd4556ab7fe6924c139b
kailerlaino <homework2265@gmail.com>
2023-09-29 19:21:00 -0500

    database

--- backend-service/sqlite3/database.py
+++ backend-service/sqlite3/database.py
@@ -1,6 +1,46 @@
 import sqlite3
 
-try: 
-    connection = sqlite3.connect('sqlite.db')
-except sqlite3.Error as Error: 
-    print (Error)
+try:
+   
+    # Connect to DB and create a cursor
+    sqliteConnection = sqlite3.connect('sql.db')
+    cursor = sqliteConnection.cursor()
+    print('DB Init')
+ 
+    # Write a query and execute it with cursor
+    query = 'select sqlite_version();'
+    cursor.execute(query)
+ 
+    # Fetch and output result
+    result = cursor.fetchall()
+    print('SQLite Version is {}'.format(result))
+
+    #cursor.execute("DROP TABLE COMPANY")
+    # Creates the database and assigns type values to variables
+    cursor.execute('''CREATE TABLE COMPANY
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT     NOT NULL,
+         STATUS         TEXT     NOT NULL,
+         DESCRIPTION    TEXT    NOT NULL);''')
+    
+    result = cursor.fetchall()
+    print('SQLite Version is {}'.format(result))
+
+    # Close the cursor
+    cursor.close()
+
+    
+
+
+ 
+# Handle errors
+except sqlite3.Error as error:
+    print('Error occurred - ', error)
+ 
+# Close DB Connection irrespective of success
+# or failure
+finally:
+   
+    if sqliteConnection:
+        sqliteConnection.close()
+        print('SQLite Connection closed')
