f24f44eb1f7bbcd6e376775fa102ca30aa30d442
krisub <krithi.s.2014@gmail.com>
2023-10-03 19:44:18 -0500

    api stuff updated

--- backend-service/core/views.py
+++ backend-service/core/views.py
@@ -4,19 +4,20 @@ from flask_cors import cross_origin
 from embedded import Relay
 import RelayContainer
 
+
 @app.route("/", methods=["GET"])
 @cross_origin()
 def index():
     dictionary1 = {
-        1: "Hello",
-        2: "World"
+        "1": "Hello",
+        "2": "World"
     }
-    print(dictionary1[1])
+    # print(dictionary1[1])
     r = RelayContainer()
     return jsonify({"message": "Home Automation API is Running!"}), 200
 
 
-@app.route("/relay/on", methods = ["GET"])
+@app.route("/relay/on", methods=["GET"])
 def relayOn():
     con = RelayContainer()
     if (con.getRelay(5)):
@@ -24,19 +25,21 @@ def relayOn():
         return jsonify({"Relay has been turned on!"})
 
 
-@app.route("relay/off", methods= ["GET"])
+@app.route("/relay/off", methods=["GET"])
 def relayOff():
     con = RelayContainer()
     if (con.getRelay(5)):
         con.getRelay(5).setState(False)
         return jsonify({"Relay has been turned off!"})
 
-@app.route("relay/add", methods=["POST"])
+
+@app.route("/relay/add", methods=["POST"])
 def addRelay():
     con = RelayContainer()
     con.addRelay(5, True)
 
-@app.route("relay/delete", methods=["POST"])
+
+@app.route("/relay/delete", methods=["POST"])
 def deleteRelay():
     con = RelayContainer()
     if (con.getRelay(5)):
