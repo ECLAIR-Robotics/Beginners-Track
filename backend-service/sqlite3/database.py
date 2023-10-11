95281bdea2831f98de96803a3058389d8bf4ac1a
PersonOOO <82835562+PersonOOO@users.noreply.github.com>
2023-10-11 11:59:31 -0500

    thing

--- backend-service/sqlite3/database.py
+++ backend-service/sqlite3/database.py
@@ -1,34 +1,31 @@
 import sqlite3
 
-
 class Database:
-    table_name = "table"  # We only want one table so dont change this often (it'll create a new one when name cchanges)
+    table_name = "table" # We only want one table so dont change this often (it'll create a new one when name cchanges)
 
     def __init__(self):
         self.con = sqlite3.connect("sql.db")
         self.cursor = self.con.cursor()
 
         # Checking to see if table even exists
-        self.cursor.execute(
-            f"SELECT name FROM sqlite_master WHERE type='table' AND name='{Database.table_name}'"
-        )
+        self.cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{Database.table_name}'")
         result = self.cursor.fetchone()
         if result:
             print(f"{self.table_name} is online.")
         else:
-            self.cursor.execute(
-                f"CREATE TABLE {Database.table_name} (id INTEGER PRIMARY KEY, state bool);"
-            )  # Uncomment this line and fill out thing
+            self.cursor.execute(f"CREATE TABLE {Database.table_name} (id INTEGER PRIMARY KEY, state bool);") # Uncomment this line and fill out thing 
             print(f"The table '{self.table_name}' is online and instantiated ")
-
-    def contains(self, id: int) -> bool:
+        
+    
+    def contains(self, id : int) -> bool:
         """
         Return
             :return True if a device entity with the 'id' exists
         """
         self.cursor.execute(f"SELECT id FROM {Database.table_name} WHERE id={id}")
+        
 
-    def add(self, id: int, state: bool):
+    def add(self, id : int, state : bool):
         """
         Adds a device entity into the database
 
@@ -39,22 +36,26 @@ class Database:
         Raises:
             ValueError: If another device with the same ID already exists in the database
         """
-
-    def getState(self, id: int) -> bool:
+        
+    
+    def getState(self, id : int) -> bool:
         """
         Returns the state of the device with the specific id
 
         Args:
             :param id of the device
-
+        
         Return:
             :return the state of the device
 
         Raises:
             ValueError: If a device with the id doesn't exist
         """
+       
 
-    def remove(self, id: int) -> bool:
+    
+    def remove(self, id : int) -> bool:
+
         """
         Drops a device with the passed in id
 
@@ -67,6 +68,7 @@ class Database:
         Return:
             :return True if device was successfully dropped
         """
+    
 
     def setState(self, id: int, state: bool) -> bool:
         """
@@ -80,7 +82,8 @@ class Database:
         Raises:
             ValueError: If a device with the id doesn't exist
         """
-
+    
+    
     def getAllDevices(self) -> list:
         """
         Returns all the devices in the database
@@ -88,11 +91,17 @@ class Database:
         Return:
             :return list of all the devices in the database
         """
+        
 
-    """
+    
+
+
+    '''
         We will talk about next time:
         try-catch
         python formating
         parameter and return checking
         
-    """
+    '''
+
+
