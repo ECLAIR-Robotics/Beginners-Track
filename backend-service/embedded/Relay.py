5c67f817d9afe19466c425d476cdc01a2e4faa9d
krisub <krithi.s.2014@gmail.com>
2023-10-17 18:47:57 -0500

    updated

--- backend-service/embedded/Relay.py
+++ backend-service/embedded/Relay.py
@@ -1,25 +1,25 @@
 import RPi.GPIO as GPIO
-import time 
+import time
+
 
 class Relay:
-    
+
     def __init__(self, relay_id, gpio_pin):
         self.relay_id = relay_id
         self.gpio_pin = gpio_pin
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_id, GPIO.OUT)
         GPIO.output(self.relay_id, GPIO.LOW)
 
-    def setState (self, state: bool):
+    def setState(self, state: bool):
         self.state = state
         if (state):
             GPIO.output(self.relay_id, GPIO.HIGH)
         else:
-            GPIO.output(self.relay_id, GPIO.LOW) 
+            GPIO.output(self.relay_id, GPIO.LOW)
 
     def getID(self):
         return self.relay_id
 
     def getRelayState(self):
         return self.gpio_pin
-        
