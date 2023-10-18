b48b6ac161ab341f65c59c038e831f6f8975146c
krisub <krithi.s.2014@gmail.com>
2023-10-17 19:19:17 -0500

    u

--- backend-service/Test.py
+++ backend-service/Test.py
@@ -12,7 +12,7 @@ if (__name__ == "__main__"):
     rc = RelayContainer.RelayContainer()
 
     rc.addCreatedRelay(r)
-    rc.addRelay(22, True)
+    rc.addRelay(27, False)
     print("#2: ", rc.str())
 
     print("#3 ", rc.getRelayIndex(0).getID())
