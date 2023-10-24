9dd392b6a96cde13d7c8fe107014ee273f2f88c8
krisub <krithi.s.2014@gmail.com>
2023-10-24 16:09:29 -0500

    READ NOTES pls

--- frontend-service/my-app/src/App.tsx
+++ frontend-service/my-app/src/App.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import TestImage from './static/Test.png'
-import {useState, useEffect} from 'react';
 import './App.css';
 import './Maid (well, it IS a home automation system, and who takes care of homes).png'
 import '../styles/card.css';
 import TestButton from './components/TestButton'
+import { useState, useEffect } from 'react';
 
 
 /*
 https://www.pluralsight.com/guides/how-to-use-the-map()-function-to-export-javascript-in-react?clickid=ztZzYd3-OxyPRO3XIcV5J3kyUkFToOWX0z5yUo0&irgwc=1&mpid=29332&aid=7010a000001xAKZAA2&utm_medium=digital_affiliate&utm_campaign=29332&utm_source=impactradius
 https://legacy.reactjs.org/docs/lists-and-keys.html
 
-get rid of all the test buttons --> have one map function and add a button for element in the array of relays
-for each button created --> need to be able to remove if necessary and rename
+get rid of ALL the test buttons --> have one map function and add a button for element in the array of relays
+
+for each button created --> need to be able to remove if necessary (POST request to /relay/delete)
+
+what if client wants to add relay? how to get that input?
+  - some button that sends a POST request to /relay/add
+
+
+NOTES:
+to test if backend api is working, need to ssh in raspi: 
+  user: krithi@ipaddress
+  pw: 12345678
+    cd Beginners-Track/backend-service
+    python3 -m base
+  then go back to your local terminal and run npm start in my-app
+  
+  if backend api isn't working pls write down which requests you aren't able to fulfill and lmk
+  if there's anything weird going on
+    see api requests in backend-service/core/views.py
+  
  */
 function App() {
   return (
@@ -38,29 +56,6 @@ export default App;
 
 /*
 function App() {
-  const [data, setData] = useState({message: ""})
-  const [clicked, setClicked] = useState(false);
-  useEffect(() => { 
-    fetch(`http://10.159.65.135:5000/`)
-      .then(res => res.json())
-      .then(data => setData(data))
-  }, []);
-  useEffect(() => {
-    console.log(data)
-  }, [data]);
-
-  const handleClick = () => { 
-    setClicked(!clicked);
-    console.log(clicked)
-  };
-  // React.useEffect(() => {
-  //   console.log(data)
-  // }, [data]);
-
-  // return (
-  //   
-  // );
-
   return (
     <div className="App">
       <header className="App-header">
@@ -76,6 +71,7 @@ function App() {
         >
           Learn React
         </a>
+        <img src={TestImage} className="Funny" alt="Maid"/>
       </header>
     </div>
   );
