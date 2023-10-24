5050eff70e1433908fdd022eec73a793d7b3c9df
PersonOOO <82835562+PersonOOO@users.noreply.github.com>
2023-10-24 17:56:07 -0500

    Starting viewspy

--- backend-service/core/views.py
+++ backend-service/core/views.py
@@ -1,8 +1,9 @@
 from flask import jsonify, request
 from core import app
+from Embedded.RelayContainer import RelayContainer
 from flask_cors import cross_origin
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"]) # route is a decorator
 @cross_origin()
 def index():
     return jsonify({"message": "Home Automation API is Running!"}), 200
