8b91a9b27376ca5a4ce23aecee9766112b400294
PersonOOO <82835562+PersonOOO@users.noreply.github.com>
2023-11-03 18:45:57 -0500

    stuff

--- frontend-service/my-app/src/components/BaseCardContainer.tsx
+++ frontend-service/my-app/src/components/BaseCardContainer.tsx
@@ -4,7 +4,7 @@ import BaseCard from './BaseCard';
 
 function BaseCardContainer() {
   return (
-   <div className = "grid-cols-3 gap-4">
+   <div className = "grid grid-cols-3 gap-4">
     <BaseCard Name = "Light1" Text = "Light2" ID = {3}/>
     <BaseCard Name = "Light1" Text = "Light2" ID = {3}/>
     <BaseCard Name = "Light1" Text = "Light2" ID = {3}/>
