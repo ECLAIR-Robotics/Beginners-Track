291e9e4cdb9d0bb12fd6d063ac57289cf4d24425
PersonOOO <82835562+PersonOOO@users.noreply.github.com>
2023-11-03 20:19:11 -0500

    button grid fixed (old button css had extra pad)

--- frontend-service/my-app/src/components/ButtonGrid.tsx
+++ frontend-service/my-app/src/components/ButtonGrid.tsx
@@ -21,18 +21,18 @@ function ButtonGrid(props: Props) {
   };
 
   return (
-    <div className="Grid-Container">
+    <div className="Grid-Container flex">
       <div className="row">
         <h2 id="b1"> {state}</h2>
       </div>
 
       <div className="grid grid-cols-2 gap-0">
-        <div className="flex flex-wrap justify-evenly gap-y-0">
+        <div className="flex flex-wrap shrink justify-evenly gap-y-0">
           <button onClick={toggleOn} className="on">
             On
           </button>
         </div>
-        <div className="flex flex-wrap justify-evenly gap-y-0">
+        <div className="flex flex-wrap shrink justify-evenly gap-y-0">
           <button onClick={toggleOff} className="off">
             Off
           </button>
