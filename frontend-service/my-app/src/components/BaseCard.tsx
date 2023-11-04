307d76b1cfe71415bb7b423adb0fdc25d1c79e63
rykhrgrissom <126636696+rykhrgrissom@users.noreply.github.com>
2023-11-03 19:55:44 -0500

    base card

--- frontend-service/my-app/src/components/BaseCard.tsx
+++ frontend-service/my-app/src/components/BaseCard.tsx
@@ -13,13 +13,13 @@ function BaseCard({Name, Text, ID} : Props) {
   
   return (
     <div className = "" >
-        <h1 className = "text-base text-blue-600 bg-red-900 border-2 border-red-900 rounded-t">    
+        <h1 className = "text-xl text-blue-600 bg-red-900 border-2 border-red-900 rounded-t">    
            {Name} 
         </h1>
-        <p className = "text-base text-violet-600 bg-red-900 border-2 border-red-900">
+        <p className = "pt-2 pb-8 text-lg text-violet-600 bg-red-900 border-2 border-red-900">
             {Text}
         </p>
-        <div className = "text-base text-green-600 bg-red-900 border-2 border-red-900 rounded-b">   
+        <div className = "text-base text-black-600 bg-red-900 border-2 border-red-900 rounded-b">   
         <ButtonGrid id = {ID}/>    
         </div>
     </div>
