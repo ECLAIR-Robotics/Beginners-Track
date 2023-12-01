685b6454514146745fc0cfa2496f2cbbcc94c47a
PersonOOO <82835562+PersonOOO@users.noreply.github.com>
2023-11-30 19:04:27 -0600

    changing main page to look normal

--- frontend-service/my-app/src/components/MainPage.tsx
+++ frontend-service/my-app/src/components/MainPage.tsx
@@ -1,38 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Footer from './Footer';
-import ButtonGrid from './ButtonGrid';
-import TestButton from './TestButton';
-import BaseCard from './BaseCard';
-import BaseCardContainer from './BaseCardContainer';
-import { Routes, Route } from "react-router-dom"
-import ANA from './AddNewAppliances'
-import { Link, useNavigate } from 'react-router-dom';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Footer from "./Footer";
+import ButtonGrid from "./ButtonGrid";
+import TestButton from "./TestButton";
+import BaseCard from "./BaseCard";
+import BaseCardContainer from "./BaseCardContainer";
+import { Routes, Route } from "react-router-dom";
+import ANA from "./AddNewAppliances";
+import { Link, useNavigate } from "react-router-dom";
 
 function MainPage() {
-    const nav = useNavigate()
-    const changePage = () => {
-      nav('/ANA')
-    }
+  const nav = useNavigate();
+  const changePage = () => {
+    nav("/ANA");
+  };
   return (
     <div>
-      {/* <TestButton title = {"Cool Button"} statingValue= {5}/>remove this and import when you are done using it*/ }
-        {/* <TestButton title = {"Bad Button"} statingValue= {25}/>remove this and import when you are done using it*/ }
-        {/* <ButtonGrid id={1}/>
+      {/* <TestButton title = {"Cool Button"} statingValue= {5}/>remove this and import when you are done using it*/}
+      {/* <TestButton title = {"Bad Button"} statingValue= {25}/>remove this and import when you are done using it*/}
+      {/* <ButtonGrid id={1}/>
         <ButtonGrid id={2}/>
         <ButtonGrid id={3}/> */}
-        <h1 className="text-3xl font-bold underline text-green-800">The thing is long </h1>
-        <BaseCardContainer/>
-        <Footer/> 
-        <div>
-        <Link to="/ANA" onClick={changePage}>Click to view the ANA page</Link> 
-        <button onClick={changePage} className='hover:text-green-900 transition-all'>Go to ANA</button>
-        </div>
+      <h1 className="text-3xl font-bold underline text-red-950">
+        Remote electrical controller{" "}
+      </h1>
+      <BaseCardContainer />
+      <Footer />
+      <div>
+        <Link to="/ANA" onClick={changePage}>
+          Click to view the ANA page
+        </Link>
+        <button
+          onClick={changePage}
+          className="hover:text-green-900 transition-all"
+        >
+          Go to ANA
+        </button>
+      </div>
     </div>
-
   );
-} 
+}
 
 export default MainPage;
-
